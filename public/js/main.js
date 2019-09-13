fa5e6fdc87788eb084ab98220463eae89c9e3e44
Talita Azevedo <talita.azevedo360@gmail.com>
2019-09-13 12:52:31 -0300

    Aula de eventos => adicionando eventos nos contadores de palavras e caracteres com jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,25 @@
 console.log("### BORA APRENDER ###");
+
+let frase = $(".frase").text();
+let numeroDePalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numeroDePalavras);
+
+
+//contadores 
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+
+    
+    console.log("##cliquei no campo###");
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length;
+    $("#contador-palavras-digitadas").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+    
+});
+
+
+
