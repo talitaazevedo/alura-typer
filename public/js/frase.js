a80d61ddaf81a1ddea0f60a35dc9d2a391dae098
Talita Azevedo <talita.azevedo360@gmail.com>
2019-09-18 10:37:57 -0300

    Implementação do botão  que busca frases por ID

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
 
@@ -22,4 +23,30 @@ function trocaFraseAleatoria(data){ //function recebe data como argumento, este 
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);//este numero aleatorio é passado num array como indice o .tempo ou .texto acessa a propriedade e retorna ela onde solicitei.
+}
+
+function buscaFrase(){
+    console.log('Busquei a frase');
+    $('#spinner').toggle();
+    let fraseId = $('#frase-id').val();
+    let dados = { id: fraseId};
+    $.get('http://localhost:3000/frases',dados,trocaFrase)
+    .fail(function(){
+        $('#erro').toggle();
+        setTimeout(function(){
+            $('#erro').toggle();
+        },2000);
+    })
+    .always(function(){
+        $('#spinner').toggle();
+    });
+}
+//está function recebe o retorno do metodo get que chama para troca de frase
+function trocaFrase(data){
+    console.log("Retorno da requisição:" + data);
+    let frase = $('.frase');
+    frase.text(data.texto);//insere no campo frase o objeto texto dawta do id.
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);//atualiza buscando o objeto tempo dentro ddo retorno da requisição assincrona
+
 }
