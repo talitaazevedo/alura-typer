8756ab92a97ba55b3854ac32a4a1d99ae6274c1d
Talita Azevedo <talita.azevedo360@gmail.com>
2019-09-18 09:57:17 -0300

    Adicionando o Spinner e tratando erros com Jquery Aula 03 concluida

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,13 +1,25 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    $.get('http://localhost:3000/frases', trocaFraseAleatoria);
+
+    $('#spinner').toggle();
+    $.get('http://localhost:3000/frases', trocaFraseAleatoria)// Metodo para fazer requisição  no servidor
+    .fail(function(){
+        $('#erro').toggle(); // Quando tiver falha mostra mensagem de erro
+        setTimeout(function(){
+            $('#erro').toggle();// esconde meu erro ou mostra
+        }, 2000);
+    })
+    .always(function(){
+        $('#spinner').toggle(); //Mostra ou esconde meu Spinner sempre executa está função o tempo todo.
+
+    });
 
 }
-function trocaFraseAleatoria(data){
+function trocaFraseAleatoria(data){ //function recebe data como argumento, este argumento é referente ao retorno do metodo get
     let frase = $('.frase');
-    let numeroAleatorio = Math.floor(Math.random() * data.length);
+    let numeroAleatorio = Math.floor(Math.random() * data.length); //Math.floor arredonda  o numero abaixo, math.random faz o randomico de um numero aleatorio
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
-    atualizaTempoInicial(data[numeroAleatorio].tempo);
+    atualizaTempoInicial(data[numeroAleatorio].tempo);//este numero aleatorio é passado num array como indice o .tempo ou .texto acessa a propriedade e retorna ela onde solicitei.
 }
